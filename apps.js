8f9b5cc74d7e5c0d2bb2c116c3fc5c0c6d36191f
David Nyambiya <davidtmn777@gmail.com>
2026-04-21 17:59:48 +0200

    Issue creation

--- apps.js
+++ apps.js
@@ -57,3 +57,64 @@ function renderProjects() {
 
     });
 }
+
+//Populating the select statements with data
+function populatesSelectOptions(){
+    const people = getFromStorage('people');
+    const projects = getFromStorage('projects');
+
+    const reporter = document.getElementById('reporter');
+    const assignment = document.getElementById('assignment');
+    const projectSelect = document.getElementById('project');
+    
+    reporter.innerHTML = '';
+    assignment.innerHTML = '<option value="">-- Unassigned --</option>';
+        projectSelect.innerHTML = '';
+
+    people.forEach(pe => {
+        const label = '${pe.name} ${pe.surname} ${pe.username}'
+        reporter.appendChild(new Option(label, pe.id));
+        assignment.appendChild(new Option(label, pe.id));
+    })
+    projects.forEach(pr =>{
+        projectSelect.appendChild(new Option(pr.name, pr.id));
+    });
+}
+
+//The issue form handler
+document.getElementById('ticketForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    const issues = getFromStorage('issues') || [];
+    const newIssue = {
+        id: generatedId('issue'),
+        summary: document.getElementById('summary').value.trim(),
+        description: document.getElementById('description').value.trim(),
+        reporter: document.getElementById('reporter').value,
+        assignment: document.getElementById('assignment').value,
+        project: document.getElementById('project').value,
+        entryDate: document.getElementById('entryDate').value,
+        targetResolutionDate: document.getElementById('targetDate').value,
+        actualResolutionDate: document.getElementById('actualDate').value,
+        status: document.getElementById('status').value,
+        priority: document.getElementById('priority').value,
+        resolutionSummary: document.getElementById('resolutionSummary').value.trim()
+    }
+        issues.push(newIssue);
+        saveToStorage('issues', issues);
+        renderIssues();
+        alert('Ticket Issue Created');
+        populatesSelectOptions();
+});
+//Determines the status of an issue
+function determineStatus(issue){
+    if (issue.status === 'resolved' || issue.actualDate) return 'resolved';
+    if (issue.targetDate) {
+        const now = new Date();
+        const targetDate = issue.targetDate;
+        if (now > targetDate) return 'overdue';
+    }
+    return open;
+    
+}
+
+//
